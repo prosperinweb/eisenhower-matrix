81235d0d761e6f165c1731cb2e88db74ca26e713
Prosper <prosperinweb@gmail.com>
2020-03-25 22:58:13 +0200

    Added drag and drop functionality

---
+++ app.js
@@ -0,0 +1,65 @@
+const forms = document.querySelectorAll("form");
+const listContainers = document.querySelectorAll(".list-container");
+const taskItems = document.querySelectorAll(".task-item");
+
+forms.forEach(form => {
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    const taskInput = form.firstElementChild;
+    const listContainer = form.nextElementSibling;
+    if (taskInput.value.trim()) {
+      const taskItem = document.createElement("li");
+      taskItem.setAttribute("draggable", true);
+      taskItem.setAttribute("class", "task-item");
+      taskItem.textContent = taskInput.value;
+      taskInput.value = "";
+      listContainer.appendChild(taskItem);
+    } else {
+      taskInput.value = "";
+    }
+  });
+});
+
+let sourceContainerId;
+
+function dragStart(e) {
+  // IE does not support text/plain
+  try {
+    e.dataTransfer.setData("text/plain", e.target.id);
+  } catch (error) {
+    e.dataTransfer.setData("Text", e.target.id);
+  }
+  sourceContainerId = this.parentElement.id;
+}
+
+function dropped(e) {
+  let id;
+  console.log("Source: " + sourceContainerId);
+  console.log("Target: " + this.id);
+  if (this.id !== sourceContainerId) {
+    cancel(e);
+    try {
+      id = e.dataTransfer.getData("text/plain");
+    } catch (error) {
+      id = e.dataTransfer.getData("Text");
+    }
+    let taskId = `#${id}`;
+    e.target.appendChild(document.querySelector(taskId));
+  }
+}
+
+function cancel(e) {
+  if (e.preventDefault) e.preventDefault();
+  if (e.stopPropagation) e.stopPropagation();
+  return false;
+}
+
+taskItems.forEach(taskItem => {
+  taskItem.addEventListener("dragstart", dragStart);
+});
+
+listContainers.forEach(listContainer => {
+  listContainer.addEventListener("drop", dropped);
+  listContainer.addEventListener("dragenter", cancel);
+  listContainer.addEventListener("dragover", cancel);
+});
